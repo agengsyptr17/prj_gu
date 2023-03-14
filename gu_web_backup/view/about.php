3da2d03d5f528947d8f14da698888fd8ad0e2de0
agengsyptr17 <88811705+agengsyptr17@users.noreply.github.com>
2023-03-14 07:15:30 +0700

    Delete gu_web_backup directory

--- gu_web_backup/view/about.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'header.php';
-include 'navbar.php';
-?>
-<br>
-<br>
-<h1 style="text-align: center;">ABOUT</h1>
--- gu_web_backup/view/footer.php
+++
@@ -1,2 +0,0 @@
-<p align="center" style="font-family: 'Courier New', Courier, monospace; font-size: 8pt; padding-top: 10px">Kantor Pertanahan Kabupaten Simalungun @<?=$year = date("Y"); ?></p>
-    
